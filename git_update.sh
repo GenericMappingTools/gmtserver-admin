cb84c1437615778feade6354761dce571667e521
Dongdong Tian <seisman.info@gmail.com>
2019-09-27 15:12:09 -0400

    Update comments in git_update.sh (#5)

--- git_update.sh
+++ git_update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Run git pull on gmtserver and determine if any file was updated.
 # If so, run the update_file_hash.sh script as well
 # P. Wessel, Sept. 26. 2019
@@ -7,12 +7,14 @@
 cd /export/gmtserver/gmt/data/gmtserver-admin
 # Check changes and update file hash
 # 2. Make sure we are on the master branch
+git checkout master
+# 3. Fetch from the remote repository
 git fetch origin master
-# 3. Do a dry run to determine if files will be updated
+# 4. Check if the local master branch is behind the remote one
 count=`git rev-list master ^origin/master --count`
-if [ "$count" -ne "0" ]; then	# 4. There will be updates
-	# 4a Update the local repo
+if [ "$count" -ne "0" ]; then	# 5. There will be updates
+	# 5a Update the local repo
 	git pull origin master
-	# 4a Update the hash table
+	# 5b Update the hash table
 	bash update_file_hash.sh
 fi
