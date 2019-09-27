f28eccc8430d1a569ce9dc095f69558b024d35d2
Paul Wessel <pwessel@hawaii.edu>
2019-09-27 08:38:04 -1000

    Determine if local repo needs update via dry-run (#4)
    * Determine if local repo needs update via dry-run
    
    Per https://stackoverflow.com/questions/7938723/git-how-to-check-if-a-local-repo-is-up-to-date/7939193, use fetch --dry-run.
    
    * Update git_update.sh
    
    Co-Authored-By: Dongdong Tian <seisman.info@gmail.com>

--- git_update.sh
+++ git_update.sh
@@ -3,12 +3,16 @@
 # If so, run the update_file_hash.sh script as well
 # P. Wessel, Sept. 26. 2019
 
-# Change directory to top dir of working directory
+# 1. Change directory to top dir of working directory
 cd /export/gmtserver/gmt/data/gmtserver-admin
 # Check changes and update file hash
+# 2. Make sure we are on the master branch
 git fetch origin master
-count=`git rev-list HEAD..origin/master --count`
-if [ "$count" -ne "0" ]; then
-    git pull origin master
-    bash update_file_hash.sh
+# 3. Do a dry run to determine if files will be updated
+count=`git rev-list master ^origin/master --count`
+if [ "$count" -ne "0" ]; then	# 4. There will be updates
+	# 4a Update the local repo
+	git pull origin master
+	# 4a Update the hash table
+	bash update_file_hash.sh
 fi
