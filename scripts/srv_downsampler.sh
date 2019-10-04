200a9e29c03f9de8b2d2b7f74a5ef5ab15a32bed
Paul Wessel <pwessel@hawaii.edu>
2019-10-03 18:01:52 -1000

    Add z name and unit
    Works except if filename contains a +x for some letter x.

--- scripts/srv_downsampler.sh
+++ scripts/srv_downsampler.sh
@@ -12,29 +12,31 @@ if [ $# -eq 0 ]; then
 fi
 
 if [ `uname -n` = "gmtserver" ]; then	# Doing official work on the server
-	HOME=/export/gmtserver/gmt/data/gmtserver-admin
+	TOPDIR=/export/gmtserver/gmt/data/gmtserver-admin
 	HERE=`pwd`
 elif [ -d ../scripts ]; then	# On your working copy, probably in scripts
 	HERE=`pwd`
 	cd ..
-	HOME=`pwd`
+	TOPDIR=`pwd`
 elif [ -d scripts ]; then	# On your working copy, probably in top gmtserver-admin
 	HERE=`pwd`
-	HOME=`pwd`
+	TOPDIR=`pwd`
 else
 	echo "error: Run srv_downsampler.sh from scripts or top gmtserver-admin directory"
 	exit -1
 fi
 # 1. Move into the staging directory
-cd ${HOME}/staging
+cd ${TOPDIR}/staging
 	
 # 2. Get recipe full file path
-RECIPE=$HOME/recipes/$1
+RECIPE=$TOPDIR/recipes/$1
 
 # 3. Extract parameters into a shell include file and ingest
 grep SRC_FILE $RECIPE   | awk '{print $2}'  > /tmp/par.sh
-grep SRC_NAME $RECIPE   | awk '{print $2}' >> /tmp/par.sh
+grep SRC_TITLE $RECIPE  | awk '{print $2}' >> /tmp/par.sh
 grep SRC_RADIUS $RECIPE | awk '{print $2}' >> /tmp/par.sh
+grep SRC_NAME $RECIPE   | awk '{print $2}' >> /tmp/par.sh
+grep SRC_UNIT $RECIPE   | awk '{print $2}' >> /tmp/par.sh
 grep DST_NODES $RECIPE  | awk '{print $2}' >> /tmp/par.sh
 grep DST_PREFIX $RECIPE | awk '{print $2}' >> /tmp/par.sh
 grep DST_FORMAT $RECIPE | awk '{print $2}' >> /tmp/par.sh
@@ -56,7 +58,7 @@ fi
 # 6. Extract the requested resolutions
 grep -v '^#' $RECIPE > /tmp/res.lis
 # 7. Replace underscores with spaces in the title
-TITLE=`echo ${SRC_NAME} | tr '_' ' '`
+TITLE=`echo ${SRC_TITLE} | tr '_' ' '`
 
 # 8. Loop over all the resolutions found
 while read RES UNIT MASTER; do
@@ -73,17 +75,25 @@ while read RES UNIT MASTER; do
 		echo "Bad resolution $RES - aborting"
 		exit -1
 	fi
+	if [ ! ${RES} = "01" ]; then	# Use plural unit
+		UNIT_NAME="${UNIT_NAME}s"
+	fi
 	DST_FILE=${DST_PREFIX}_${RES}${UNIT}.grd
-	if [ -f ${DST_FILE} ]; then	# Do nothing
+	txt="${TITLE} at ${RES} arc ${UNIT_NAME}"
+	if [ -f re${DST_FILE} ]; then	# Do nothing
 		echo "${DST_FILE} exist - skipping"
 	elif [ "X${MASTER}" = "Xmaster" ]; then # Just make a copy of the master
-		gmt grdconvert ${SRC_FILE} ${DST_FILE}=${DST_FORMAT} --IO_NC4_DEFLATION_LEVEL=9
-		gmt grdedit ${DST_FILE} -D+t"${TITLE} at ${RES} arc ${UNIT_NAME}"+r"Reformatted from master file ${SRC_ORIG}"
+		echo "Convert ${SRC_FILE} to ${DST_FILE}=${DST_FORMAT}"
+		#gmt grdconvert ${SRC_FILE} ${DST_FILE}=${DST_FORMAT} --IO_NC4_DEFLATION_LEVEL=9
+		remark="Reformatted from master file ${SRC_ORIG}"
+		gmt grdedit ${DST_FILE} -D+t"${txt}"+r"$remark"+z"${SRC_NAME} (${SRC_UNIT})"
 	else	# Must downsample to a lower resolution via spherical Gaussian filtering
 		# Get suitable Gaussian full-width filter rounded to nearest 0.1 km
+		echo "Down-filter ${SRC_FILE} to ${DST_FILE}=${DST_FORMAT}"
 		FILTER_WIDTH=`gmt math -Q ${SRC_RADIUS} 2 MUL PI MUL 360 DIV $INC MUL 10 MUL RINT 10 DIV =`
 		gmt grdfilter ${SRC_FILE} -Fg${FILTER_WIDTH} -D4 -I${RES}${UNIT} -r${DST_NODES} -G${DST_FILE}=${DST_FORMAT} --IO_NC4_DEFLATION_LEVEL=9 --PROJ_ELLIPSOID=Sphere
-		gmt grdedit ${DST_FILE} -D+t"${TITLE} at ${RES} arc ${UNIT_NAME}"+r"Obtained by Gaussian spherical filtering (${FILTER_WIDTH} km fullwidth) from ${SRC_FILE}"
+		remark="Obtained by Gaussian spherical filtering (${FILTER_WIDTH} km fullwidth) from ${SRC_FILE}"
+		gmt grdedit ${DST_FILE} -D+t"${txt}"+r"${remark}"+z"${SRC_NAME} (${SRC_UNIT})"
 	fi
 done < /tmp/res.lis
 # 9. Clean up /tmp
