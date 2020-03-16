d87d0f649f8e0f9cd3010ea2d2fc2952353020f1
Dongdong Tian <seisman.info@gmail.com>
2020-03-16 10:43:42 -0400

    Make srv_git_update.sh more verbose (#34)

--- scripts/srv_git_update.sh
+++ scripts/srv_git_update.sh
@@ -2,21 +2,22 @@
 # srv_git_update.sh
 #
 # Run git pull on gmtserver and determine if any file was updated.
-# If so, run the update_file_hash.sh script as well
+# If so, run the srv_update_sha256.sh script as well
 # P. Wessel, Oct. 3, 2019
 
+echo "Start Time:" $(date)
 # 1. Change directory to top dir of working directory
 cd /export/gmtserver/gmt/gmtserver-admin
 # Check changes and update file hash
 # 2. Make sure we are on the master branch
 git checkout master
 # 3. Fetch from the remote repository
-git fetch origin
+git fetch -v origin
 # 4. Check if the local master branch is behind the remote one
 count=`git rev-list master...origin/master --count`
 if [ "$count" -ne "0" ]; then	# 5. There will be updates
 	# 5a Update the local repo
-	git pull origin master
+	git pull -v origin master
 	# 5b Do rsync of files than may have changed to data/cache
 	rsync -a --delete cache ../data
 	# 5c Update the SHA256 hash table
@@ -24,3 +25,5 @@ if [ "$count" -ne "0" ]; then	# 5. There will be updates
 	# 5d Duplicate to the Md5 file for backwardness
 	cp -f ../data/gmt_hash_server.txt ../data/gmt_md5_server.txt
 fi
+
+echo "End Time:" $(date)
