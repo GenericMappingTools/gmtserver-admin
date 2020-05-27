ab8a9dae1845a65313ffcbd4f5cb6368927a2ced
Paul Wessel <pwessel@hawaii.edu>
2020-05-26 17:57:56 -1000

    Update data server file and link script (#43)
    We decide that for GMT 6.1 any earth_relief file obtained by a link with the old name should be written to teh server/earth_relief directory on the local machine.  This is now reflected in the table.  The srv_setbackwards_links.sh removes any old links and creates them.  This script is only meant to be run manually, probably just once, but its precense is a log of what happened.

--- scripts/srv_setbackwards_links.sh
+++ scripts/srv_setbackwards_links.sh
@@ -4,10 +4,10 @@
 # Creates symbolic links to the earth_relief/earth_relief_xxy.grd files
 # that were known prior to GMT 6.1.  For older GMT versions we need
 # symbolic links in the root (/export/gmtserver/gmt/data) directory,
-# while for GMT 6.1 we need links in the server directory instead.
-# This script creates both sets of links
+# while for GMT 6.1 we need links in the server/earth_relief directory
+# instead. This script creates both sets of links.
 
-# 1. Make the list of the resolutions and the registrations
+# 0. Make the list of the resolutions and the registrations
 cat << EOF > /tmp/tmp.lis
 01d	g
 30m	g
@@ -24,19 +24,24 @@ cat << EOF > /tmp/tmp.lis
 15s	p
 EOF
 
-# 2a. Make the links in the root dir
+# 1. Go to the root of the data service
 cd /export/gmtserver/gmt/data
+
+# 2. Delete the old links wherever they are found
+rm -f earth_relief_???.grd server/earth_relief_???.grd server/earth_relief/earth_relief_???.grd 
+
+# 3a. Make the links in the root dir
 while read xxy registration; do
 	ln -s server/earth_relief/earth_relief_${xxy}_${registration}.grd earth_relief_${xxy}.grd
 done < /tmp/tmp.lis
-# Manually do the 60m -> lis link
+# 3b. Manually do the 60m -> lis link
 ln -s server/earth_relief/earth_relief_01d_g.grd earth_relief_60m.grd
 
-# 2b. Make the links in the server dir
-cd /export/gmtserver/gmt/data/server
+# 4a. Make the links in the earth_relief dir
+cd /export/gmtserver/gmt/data/server/earth_relief
 while read xxy registration; do
-	ln -s earth_relief/earth_relief_${xxy}_${registration}.grd earth_relief_${xxy}.grd
+	ln -s earth_relief_${xxy}_${registration}.grd earth_relief_${xxy}.grd
 done < /tmp/tmp.lis
-# Manually do the 60m -> lis link
-ln -s earth_relief/earth_relief_01d_g.grd earth_relief_60m.grd
+# 4b. Manually do the 60m -> lis link
+ln -s earth_relief_01d_g.grd earth_relief_60m.grd
 rm -f /tmp/tmp.lis
