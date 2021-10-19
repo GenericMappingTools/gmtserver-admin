57434ae60f0fd4f0fb19667b50c98956d3f342d8
Paul Wessel <pwessel@hawaii.edu>
2021-10-18 17:34:11 -1000

    Update srv_srtm_coverage.sh

--- scripts/srv_srtm_coverage.sh
+++ scripts/srv_srtm_coverage.sh
@@ -17,11 +17,10 @@
 #	lon, lat and add 0.5 for the center of the 1x1 degree pixel
 awk '{ printf "%s%s\t%s\%s\n", substr ($1, 5, 3), substr ($1, 4, 1), substr ($1, 2, 2), substr ($1, 1, 1)}' information/srtm_tiles.txt > /tmp/xy.txt
 gmt xyz2grd -R-180/180/-60/60 -I1 -r -fg -G/tmp/srtm.nc=nb /tmp/xy.txt -i0-1o0.5 -An -V -di0
-g
 # 2. Use the @earth_mask_15s_g grid to determine which tiles have at least some ocean in them
-gmt grdmath -R0/360/-60/60 @earth_mask_15s_p 0 EQ = /tmp/ocean.nc=nb
-gmt grd2xyz /tmp/ocean.nc | gmt xyz2grd -R-180/180/-60/60 -I1 -r -Au -G/tmp/ocean_tile.nc=ns -V -fg
+gmt grdmath -R0/360/-60/60 @earth_mask_15s_p 0 EQ = /tmp/ocean_mask.nc=nb
+gmt grd2xyz /tmp/ocean_mask.nc | gmt xyz2grd -R-180/180/-60/60 -I1 -r -Au -G/tmp/ocean.nc=ns -V -fg
 # 3. Combine the tile and 2xocean files to yield the 0,1,2 final grid
-gmt grdmath /tmp/ocean_tile.nc 2 MUL /tmp/srtm.nc ADD = srtm_tiles.nc=nb --IO_NC4_DEFLATION_LEVEL=9
+gmt grdmath /tmp/ocean.nc 2 MUL /tmp/srtm.nc ADD = srtm_tiles.nc=nb --IO_NC4_DEFLATION_LEVEL=9
 gmt grdedit srtm_tiles.nc -D+t"Availability of SRTM tiles"+r"0 means empty, 1 means land tile, 2 means partial land tile"
 echo "srtm_tiles.nc: You must manually add it to the cache and commit the change there"
