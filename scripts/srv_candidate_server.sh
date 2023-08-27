6d1aad03129d1e5bf94bfa5cc228598d9a9d4434
Paul Wessel <pwessel@hawaii.edu>
2023-08-27 16:36:04 +0200

    Update srv_candidate_server.sh

--- scripts/srv_candidate_server.sh
+++ scripts/srv_candidate_server.sh
@@ -3,19 +3,22 @@
 # then find all the planet_dataset_server.txt snippets
 # and cat into a final gmt_data_server.txt for this server.
 
-# 2. Set name of the candidate server, its directory, and URL
+# A. Set name of the candidate server, its directory, and URL
 INFO_DIR=../gmtserver-admin/information
 CANDIDATE=candidate
 CANDIDATE_DIR=/export/gmtserver/gmt/${CANDIDATE}
 CANDIDATE_SERVER=${CANDIDATE}.generic-mapping-tools.org
 CANDIDATE_SERVER_DIR=${CANDIDATE_SERVER}:${CANDIDATE_DIR}
 
-GUNK=_pre
-# Create the bash script for the candidate server to execute
+# B. If you are testing and do not want to overwrite the candidate
+# server;s gmt_data_server.txt, use DEBUG to insert text in filename
+DEBUG=
+
+# C. Create the bash script for the candidate server to execute
 cat << EOF > /tmp/candidate.sh
 #!/usr/bin/env bash
 #
-# 1. cd to candidate server
+# 1. cd to candidate server dir
 cd ${CANDIDATE_DIR}
 
 # 2. Backup last server file, if any
@@ -27,19 +30,19 @@ fi
 cp -f ${INFO_DIR}/gmt_data_server_header.txt /tmp/gmt_data_server.txt
 
 # 4. Make a list of all server snippets under server
-find server -name '*_*_server.txt' > /tmp/s.lis
+find server -name '*_*_server.txt' > /tmp/datasets.lis
 while read file; do
 	cat \${file} >> /tmp/gmt_data_server.txt
-done < /tmp/s.lis
+done < /tmp/datasets.lis
 
 # 5. Count the number of data files or directory entries and start gmt_data_server.txt:
-grep -v '^#' /tmp/gmt_data_server.txt | wc -l | awk '{print $1}' > ${CANDIDATE_DIR}/gmt_data_server${GUNK}.txt
+grep -v '^#' /tmp/gmt_data_server.txt | wc -l | awk '{print $1}' > ${CANDIDATE_DIR}/gmt_data_server${DEBUG}.txt
 
 # 6. Append all snippets once the total was written:
-cat /tmp/gmt_data_server.txt >> ${CANDIDATE_DIR}/gmt_data_server${GUNK}.txt
+cat /tmp/gmt_data_server.txt >> ${CANDIDATE_DIR}/gmt_data_server${DEBUG}.txt
 
 # 7. Cleanup
-rm -rf /tmp/gmt_data_server.txt /tmp/s.lis /tmp/candidate.sh
+rm -rf /tmp/gmt_data_server.txt /tmp/datasets.lis /tmp/candidate.sh
 EOF
 
 # Set execute permissions and place on server /tmp
