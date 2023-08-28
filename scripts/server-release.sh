ce6fc1e66a26592fea06573b05e0839ca67435f2
Paul Wessel <pwessel@hawaii.edu>
2023-08-28 18:20:22 +0200

    Update server-release.sh

--- scripts/server-release.sh
+++ scripts/server-release.sh
@@ -1,26 +1,60 @@
 #!/usr/bin/env bash
 #
-# DANGEROUS script (not tested yet) that synchronizes the files
-# in the candidate directory over to the server oceania. It
-# also copies over gmt_data_server.txt.
+# Script that copies datasets from the candidate directory over to the 
+# server oceania's directory. If no data set is specified then we copy
+# all data sets from candidate to server.
+# Optional argument is name of a single dataset [Default acts on all]
 
-TOP_DIR=/export/gmtserver/gmt		# The gmt account home dir
+# A. Set various fixed parameters
+CANDIDATE=candidate				# Directory with candidate data
+SERVER=data						# Directory with public (oceania) data
+TOP_DIR=/export/gmtserver/gmt	# The top dir of the gmt account
+#---------------------------------------------------------------------
+CANDIDATE_DIR=${TOP_DIR}/${CANDIDATE}/server			# Candidate planets are under here
+SERVER_DIR=${TOP_DIR}/${SERVER}/server					# Public planets on oceania are under here
+CANDIDATE_SERVER=${CANDIDATE}.generic-mapping-tools.org	# URL of the candidate server
+INFO_DIR=${TOP_DIR}/gmtserver-admin/information			# Directory with gmt_data_server header chunk
 
-CANDIDATE_DIR=${TOP_DIR}/candidate	# Place with updated files
-SERVER_DIR=${TOP_DIR}/data			# Public oceania server dir
-
-# 1. Check we are on the right computer!
-SERVER=$(uname -n)
-if [ ! "${SERVER}" = "gmtserver" ]; then
-	cat <<- EOF  >&2
-	server-release.sh: You must be logged onto the gmtserver, not ${SERVER} to make this target
-	EOF
-	exit 1
+# B. Determine if a single data set or everything from candidate shall be synced
+echo -n "server-release.sh: Enter a specific data set (e.g., mars_relief) or hit return for all data on the candidate: "
+read DATASET
+if [ "X${DATASET}" = "X" ]; then
+	echo "server-release.sh: Copying all data sets from candidate server to oceania"
+else
+	echo "server-release.sh: Copying ${DATASET} from candidate server to oceania"
+	DATASET="/${DATASET}"
 fi
 
-# Here we are logged onto the server and placed ourselves in the candidate server dir
+# C. Build the script to be copied and executed on the remote server
+cat << EOF > /tmp/release.sh
+#!/usr/bin/env bash
+# Script made by "make server-release" to be run on the gmtserver
+#
+# 1. Issue rsync command
+rsync -al ${CANDIDATE_DIR}${DATASET} ${SERVER_DIR}${DATASET}
+# 2. Rebuild the gmt_data_server.txt file
+# 2a. Make sed script that changes THEDATE to today's dat
+date "+%Y-%m-%d" | awk '{printf "s/THEDATE/%s/g\n", $$1}' > /tmp/sed.txt
+# 2b. Find all the dataset server files
+find ${SERVER_DIR} -name '*_*_server.txt' > /tmp/datasets.lis
+# 2c. Count the number of data files or directory entries and start first line of /tmp/gmt_data_server.txt:
+cat \$(cat /tmp/datasets.lis) | grep -v '^#' | wc -l | awk '{printf "%d\n", $1}' > /tmp/gmt_data_server.txt
+# 2d. Append the header information section after piping via sed to get the date
+cat ${TOP_DIR}/gmt_data_server_header.txt | sed -f /tmp/sed.txt >> /tmp/gmt_data_server.txt
+# 2e. Append all the data set files to the same file
+cat \$(cat /tmp/datasets.lis) >> /tmp/gmt_data_server.txt
+# 2f. Copy the old server file to the backup file
+cp -f ${TOP_DIR}/${SERVER}/gmt_data_server.txt ${TOP_DIR}/${SERVER}/gmt_data_server_previous.txt
+# 2g. Place the new server file
+cp -f /tmp/gmt_data_server.txt ${TOP_DIR}/${SERVER}
+# 3. Clean up
+rm -f tmp/sed.txt /tmp/gmt_data_server.txt /tmp/release.sh
+EOF
 
-# Sync all data files from candidate to server [UNTESTED]
-echo "rsync -al ${CANDIDATE_DIR} ${SERVER_DIR}"
-# Update the server table on oceania
-cp -f ${CANDIDATE_DIR}/gmt_data_server.txt ${SERVER_DIR}
+# Set execute permissions and place on server /tmp
+chmod +x /tmp/release.sh
+echo server-release.sh: scp /tmp/release.sh ${CANDIDATE_SERVER}:/tmp
+scp /tmp/release.sh ${CANDIDATE_SERVER}:/tmp
+
+# Execute the script via ssh on oceania
+echo server-release.sh: ssh ${CANDIDATE_SERVER} "/tmp/release.sh"
