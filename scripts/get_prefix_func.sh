1b6dee41c9a4950adb62e1fca72e13424551cb61
Paul Wessel <pwessel@hawaii.edu>
2023-12-06 20:16:23 +0100

    Update get_prefix_func.sh
    Fix so any longitude in -180/180 or 0/360 gets correct W/E tag after subtracting 360 if needed.

--- scripts/get_prefix_func.sh
+++ scripts/get_prefix_func.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Function that creates tile label in integer degrees W/S
 
-function get_prefix  () {       # Takes west (in -180/180 range) and south and makes the {N|S}yy{W|E}xxx prefix
+function get_prefix  () {       # Takes west (in -180/180 or 0/350 range) and south and makes the {N|S}yy{W|E}xxx prefix
 	# Get nearest integer degrees
-	W=$(gmt math -Q $1 RINT =)
+	W=$(gmt math -Q $1 DUP 180 GE 360 MUL SUB RINT =)	# Ensure it is in -180 <= LON < +180
 	S=$(gmt math -Q $2 RINT =)
 	if [ $W -ge 0 ]; then
 		X=$(printf "E%03d" $W)
